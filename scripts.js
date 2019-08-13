aebc750fa07e4b982e8d5d660921fa500a0c7d7f
Yevgeny Getman <yevgeny.getman@icims.com>
2019-08-13 12:53:47 -0400

    add ordinal suffix function

--- scripts.js
+++ scripts.js
@@ -704,3 +704,22 @@ function stripNonNum(str){
 	/* strip all non-numeric characters from str (leaves "." for float values) */
 	return str.replace(/[^\d.-]/g, '');
 }
+
+function ordinalSuffix(n) {
+
+	/*
+		- st is used with numbers ending in 1 (e.g. 1st, pronounced first)
+		- nd is used with numbers ending in 2 (e.g. 92nd, pronounced ninety-second)
+		- rd is used with numbers ending in 3 (e.g. 33rd, pronounced thirty-third)
+		- As an exception to the above rules, all the "teen" numbers ending with 11, 12 or 13 use -th (e.g. 11th, pronounced eleventh, 112th, pronounced one hundred [and] twelfth)
+		- th is used for all other numbers (e.g. 9th, pronounced ninth).
+	*/
+
+    var j = n % 10, k = n % 100;
+
+    if(j == 1 && k != 11) return n + "st";
+    if(j == 2 && k != 12) return n + "nd";
+    if(j == 3 && k != 13) return n + "rd";
+    
+    return n + "th";
+}
